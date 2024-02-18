f769353ec35834d478e46812d41c908ed8405747
prog-ops <akunbaru14102019@gmail.com>
2024-02-18 11:15:21 +0700

    Trigger build

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,6 @@ import Column from "./components/Column.jsx";
 function App() {
     return (
         <div className='App'>
-
             <Column state='PLANNED'/>
             <Column state='ONGOING'/>
             <Column state='DONE'/>

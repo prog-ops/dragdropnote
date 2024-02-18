fd6624b50433e6a68a08868b4f37e4839af6a774
prog-ops <akunbaru14102019@gmail.com>
2024-02-18 09:50:24 +0700

    Trigger build - rmh

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,14 @@ import './App.css'
 import Column from "./components/Column.jsx";
 
 function App() {
-  return (
-      <div className='App'>
-        <Column state='PLANNED'/>
-        <Column state='ONGOING'/>
-        <Column state='DONE'/>
-      </div>
-  )
+    return (
+        <div className='App'>
+
+            <Column state='PLANNED'/>
+            <Column state='ONGOING'/>
+            <Column state='DONE'/>
+        </div>
+    )
 }
 
 export default App
